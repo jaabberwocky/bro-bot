81df40c4a19c891b478f38945e018449cae9c371
tobias <leongtobias@gmail.com>
2021-08-14 14:37:56 +0800

    Remove extraneous comments

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ const TelegramBot = require("node-telegram-bot-api");
 const utils = require("./utils.js");
 require("dotenv").config();
 
-// replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TELEGRAM_TOKEN;
-
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 
 let coolCount = {};
